ef47997f1ca6f206eee68e7fc651d309bdb9b1d7
Slava Dmitriev <d2c.tool@gmail.com>
2019-04-04 17:59:32 +0300

    first

--- VKBotWebApp/Startup.cs
+++ VKBotWebApp/Startup.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using VkNet;
+using VkNet.Abstractions;
+using VkNet.Model;
 
 namespace VKBotWebApp
 {
@@ -26,6 +22,12 @@ namespace VKBotWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSingleton<IVkApi>(sp =>
+            {
+                var api = new VkApi();
+                api.Authorize(new ApiAuthParams { AccessToken = Configuration["Config:AccessToken"] });
+                return api;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
